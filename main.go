5dfd0333c9f90761d45cb0561561ddaa06083fb7
danielalexis <danielalexis.o.pereira@gmail.com>
2023-11-30 14:12:00 +0000

    initial copy of hestia-api

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"hestia/api/logger"
+	"hestia/api/middleware"
+	"hestia/api/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	if os.Getenv("HESTIA_ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.New()
+	if os.Getenv("HESTIA_ENV") != "production" {
+		r.Use(gin.Logger())
+	}
+	r.UseH2C = true
+	r.ForwardedByClientIP = true
+	r.RedirectTrailingSlash = false
+	r.Use(gin.Recovery())
+	r.Use(middleware.RequestIdMiddleware())
+	r.Use(middleware.DontCache())
+	r.HandleMethodNotAllowed = true
+	r.NoMethod(middleware.MethodNotAllowed())
+	r.NoRoute(middleware.NotFound())
+	routes.SetRoutes(r)
+	return r
+}
+
+func main() {
+	logger.InitLogger()
+	r := SetupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
